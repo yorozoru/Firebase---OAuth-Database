37cacee812fea6e8d7cf8b4d8ed3f944c212ff82
Dillan Pho <jaden6firs@gmail.com>
2024-11-27 11:56:50 -0500

    changed persistance to be directly in the component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 "use server"
 import OAuthButton from "@/components/OauthButton";
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
 export default async function Home() {
+  const cookieStore = cookies();
+  const userId = cookieStore.get('userId')?.value;
+
+  if (userId) {
+    redirect('/results');
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen">
       <OAuthButton/>
